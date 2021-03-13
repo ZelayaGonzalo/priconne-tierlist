50097a2b90b9b2e2ad3a5fe67a997ab95a2e67fd
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2021-03-13 17:02:37 -0300

    Updated data, added map 10 recomendations

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,35 +1,13 @@
 function Info(){
     return(
-        <div className="side-bar">
-        <section className="credits-section">
-        <h2>Princess Connect Tierlist</h2>
-        <p>Hello and welcome to this tierlist.</p>
-        <p>This list was made by TimaeuSS and you can find the original <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQlbwfpi-KflPB6aA-LE1a50xiLvWsNRU1vcusLIWwnPLzgE2iXw9Or8JXQdezN-s1rakU074hFK1Nh/pubhtml#">here</a>. I made this app for ease of navigation</p>
-        </section>
-        <section className="credits-section">
-          <h3>Instructions:</h3>
-          <h4>Second tab (Mobile):</h4>
-          <p>Touch on any place on a tab of a character to show the hidden tab with some extra info</p>
-          <h4>Sorting:</h4>
-          <p>Touch any of the sections on the top to sort the list. Name and Stars(Desktop) can be sorted in inverse order </p>
-          <h4>Filters:</h4>
-          <p>Touch the source tab on the top to open the filters window. You can apply as many filters as you want at the same time</p>
-          <h3>References:</h3>
-          <h4>Position:</h4>
-          <p>The lowest the number the more foward it'll go</p>
-          <h4>CB Rank:</h4>
-          <p>The current rank suggested for better performance on Clan Battles. (7-6 is rank 7 with full equips). </p>
-          <h4>rank Diff:</h4>
-          <p>the differences in stats between rank 7-6 and 8-3 (at 3 stars).On desktop hover over the CB rank to see rankDiff</p>
-          <h4 className="enjoy"> Enjoy :)</h4>
-        </section>
-        <footer>
-            <p>if you have any troubles feel free to contact me:</p>
-            <p>Email: <a href="zelayagonzalo33@gmail.com">zelayagonzalo33@gmail.com</a></p>
-            <p>Discord: 0PT1C0#3072 </p>
-            <p>If anyone from G.Edifice is here, greetings from 0PT1C0</p>
-        </footer>
-      </div>
+      <div className="side-bar">
+      <section className="credits-section">
+          <a href="https://priconne-tierlist.com/" className="btn"> Go Home</a>
+          <a href="https://priconne-tierlist.com/tierlist-tutorial" target="_blank" className="btn">Tutorial</a>
+          <a href="https://priconne-tierlist.com/tierlist-changelog" target="_blank" className="btn">Changelog</a>
+          <a href="https://priconne-tierlist.com/tierlist-disclaimer" target="_blank" className="btn">Disclaimer</a>
+      </section>
+    </div>
     )
 }
 

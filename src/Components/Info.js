4752116f54c5046dbf35d30b926e1da97cf4f03f
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2021-03-13 17:10:05 -0300

    Added noreferrer to blank target links

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -3,9 +3,9 @@ function Info(){
       <div className="side-bar">
       <section className="credits-section">
           <a href="https://priconne-tierlist.com/" className="btn"> Go Home</a>
-          <a href="https://priconne-tierlist.com/tierlist-tutorial" target="_blank" className="btn">Tutorial</a>
-          <a href="https://priconne-tierlist.com/tierlist-changelog" target="_blank" className="btn">Changelog</a>
-          <a href="https://priconne-tierlist.com/tierlist-disclaimer" target="_blank" className="btn">Disclaimer</a>
+          <a href="https://priconne-tierlist.com/tierlist-tutorial" target="_blank" rel="noopener noreferrer" className="btn">Tutorial</a>
+          <a href="https://priconne-tierlist.com/tierlist-changelog" target="_blank" rel="noopener noreferrer" className="btn">Changelog</a>
+          <a href="https://priconne-tierlist.com/tierlist-disclaimer" target="_blank" rel="noopener noreferrer" className="btn">Disclaimer</a>
       </section>
     </div>
     )
